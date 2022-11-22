6e44016d2f8be5c619b787e66d503ce6bdfae7ee
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-11-21 20:37:47 -0600

    initial commit

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+rooms = [
+    {'id': 1, 'name': "Let's learn Python"},
+    {'id': 2, 'name': "Learn Vue.js"},
+    {'id': 3, 'name': "Build with Django"}
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request):
+    return render(request, 'base/room.html')
