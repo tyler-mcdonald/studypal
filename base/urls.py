275432421a735b92d7a4ca473ea08d2bd1a2441d
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-11-21 21:38:45 -0600

    comment

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # name parameter allows dynamic insertion of the path into template
     path('room/<str:pk>', views.room, name='room')
 ]
